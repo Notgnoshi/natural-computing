dbb1cfff752a564d34af57c85b47e20638913667
Austin Gill <notgnoshi@gmail.com>
2019-03-29 18:20:19 -0600

    Check given text symbols for unknown symbols.

--- homework/fractals/fractals/grammar/grammar.py
+++ homework/fractals/fractals/grammar/grammar.py
@@ -10,8 +10,15 @@ class Grammar:
         self.productions = productions
         self.symbols = frozenset(filter(str.isalpha, productions.keys()))
 
+    def __check_text_symbols(self, text):
+        """Ensure the given text contains only known symbols."""
+        for symbol in filter(str.isalpha, text):
+            if symbol not in self.symbols:
+                raise ValueError(f"Unknown symbol '{symbol}'")
+
     def apply(self, text):
         """Apply the production rules to the given text."""
+        self.__check_text_symbols(text)
         return text
 
     def iapply(self, axiom):
